6c582823a5b818d1542ddc31a966009bf6983ae6
WillMS <aquatixo@naver.com>
2023-04-03 22:49:23 +0900

    carousel

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -3,6 +3,7 @@ import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
 import img4 from '../images/4.png'
 import img6 from '../images/6.png'
+import img10 from '../images/10.png'
 import Home from '@mui/icons-material/Home';
 
 const paperStyle = {
@@ -15,12 +16,18 @@ const HomeCarousel = () => {
           name: "Random Name #1"
           ,description: "Probably the most random thing you have ever seen!"
           ,img : img4
-      },
-      {
+      }
+      ,{
           name: "Random Name #2"
           ,description: "Hello World!"
           ,img : img6
       }
+      ,{
+        name: "Random Name #3"
+        ,description: "Aqua God!"
+        ,img : img10
+      }
+
   ]
   
   return (
@@ -34,10 +41,9 @@ const HomeCarousel = () => {
 function MyCarousel(props){
   return (
     <Carousel 
-      animation='slide'
-
+      animation='fade'
       // IndicatorIcon={<Home/>}
-
+      // navButtonsAlwaysVisible='true'
       indicatorIconButtonProps={{
         style: {
             // padding: '0px',    // 1
@@ -53,7 +59,7 @@ function MyCarousel(props){
 
       indicatorContainerProps={{
         style: {
-            marginTop: '30px', // 5
+            marginTop: '0px', // 5
             textAlign: 'center' // left center right
         }
 
@@ -75,11 +81,33 @@ function Item(props)
 {
     return (
       <Paper sx={{ boxShadow: 0 }}>
+          <div style={{display:'flex'
+            , justifyContent:"center"
+            , alignItems: "center"
+          }}>
+            <img 
+              src={props.item.img} 
+              style={{
+                // width:'100%',
+                height: '50vh'
+              }}
+            />
 
-          <img src={props.item.img} style={{height: '300px'}}/>
+            <div style={{
+                position:'absolute'
+                ,bottom:0
+                ,backgroundColor:'white'
+                ,opacity: 0.7
+                ,padding:'10px'
+                ,borderRadius: '30px'
+              }}
+            >
+              <h2 style={{}}>{props.item.name}</h2>
+              <p>{props.item.description}</p>
+            </div>
+          </div>
 
-          <h2>{props.item.name}</h2>
-          <p>{props.item.description}</p>
+          
           {/* <Button className="CheckButton">
               Check it out!
           </Button> */}
