0937f65d7dce87ea2366062935cb90ede3eaa14c
WillMS <aquatixo@naver.com>
2023-04-03 22:14:07 +0900

    carousel

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -25,25 +25,61 @@ const HomeCarousel = () => {
   
   return (
     <div>
-      <Carousel 
-        animation='slide'
-      >
-        {
-          items.map( (item, i) => <Item key={i} item={item} /> )
-        }
-      </Carousel>
-
+      <MyCarousel items={items} />
+      
     </div>
+  )
+}
+
+function MyCarousel(props){
+  return (
+    <Carousel 
+      animation='slide'
+
+      // IndicatorIcon={<Home/>}
+
+      indicatorIconButtonProps={{
+        style: {
+            // padding: '0px',    // 1
+            //color: 'blue'       // 3
+        }
+      }}
+
+      activeIndicatorIconButtonProps={{
+        style: {
+            backgroundColor: 'blue' // 2
+        }
+      }}
+
+      indicatorContainerProps={{
+        style: {
+            marginTop: '30px', // 5
+            textAlign: 'center' // left center right
+        }
+
+      }}
+
+      next={ () => {console.log('next')} }
+      prev={ () => {console.log('prev')} }
+      // NextIcon={<img src={img4}/>}
+      // PrevIcon={<img src="http://random.com/prev"/>}
+    >
+      {
+        props.items.map( (item, i) => <Item key={i} item={item} /> )
+      }
+    </Carousel>
   )
 }
 
 function Item(props)
 {
     return (
-      <Paper style={paperStyle} >
+      <Paper sx={{ boxShadow: 0 }}>
+
+          <img src={props.item.img} style={{height: '300px'}}/>
+
           <h2>{props.item.name}</h2>
-          {/* <p>{props.item.description}</p> */}
-          {/* <img src={props.item.img} className="center-fit center"/> */}
+          <p>{props.item.description}</p>
           {/* <Button className="CheckButton">
               Check it out!
           </Button> */}
