09db8c26d3d23b48507928a6be0c54f8a974b015
WillMS <aquatixo@naver.com>
2023-04-12 22:09:11 +0900

    product cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomeCarousel from './HomeCarousel';
+import ProductCards from './ProductCards';
 
 const Home = () => {
   return (
     <>
       <HomeCarousel/>
-      
+      <ProductCards/>
     </>
   )
 }
